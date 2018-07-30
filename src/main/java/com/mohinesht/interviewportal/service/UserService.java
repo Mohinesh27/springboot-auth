45f3bbb1a63d60f49f107c65d13ba1036d3ce807
mohinesh Tayade <mohineshtayade123@gmail.com>
2018-07-30 15:11:29 +0530

    Wrong files

--- src/main/java/com/mohinesht/interviewportal/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.mohinesht.interviewportal.service;
-
-import com.mohinesht.interviewportal.models.User;
-
-public interface UserService {
-	public User findUserByEmail(String email);
-	public void saveUser(User user);
-}
