3f66aad9e51c8cd744da175ddfbd67cf8b1fb210
HQDOMAIN\CWR.MOHINESH.TAYADE <cwr.mohinesh.tayade@asurion.com>
2018-07-30 12:38:37 +0530

    Remove wrong files

--- src/main/java/com/rise/interviewportal/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.rise.interviewportal.service;
-
-import com.rise.interviewportal.models.User;
-
-public interface UserService {
-	public User findUserByEmail(String email);
-	public void saveUser(User user);
-}
